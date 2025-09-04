7f573185f59165ffcfe5d78b7da760a35d414046
SeungYeonJeong22 <jq3210@gmail.com>
2025-09-04 18:01:04 +0900

    A:Functions/chat.py

---
+++ Functions/chat.py
@@ -0,0 +1,24 @@
+from Settings.apis import *
+from ..Settings.router import APIRouter
+from Utils.utils import parse_api_response, extract_api_params_from_user_query
+
+def chat(query, agent=None):
+    if agent is None or agent.chain is None:
+        raise ValueError("Agent is Not Initialization")
+
+    router = APIRouter()
+    api = router.route(query)
+
+    if api:
+        user_params = extract_api_params_from_user_query(query, agent, api)
+    
+    api_response = api.search(user_params)
+    parsed_api_response = parse_api_response(api_response)
+
+    response = agent.chain.invoke({
+        "system": agent.get_system_message(),
+        "input": query,
+        "api_result": parsed_api_response
+    })
+
+    return response.content
